99d225f6b8826c7c1df73da54f4a5e66849efcc0
armagedontkd <117161052+armagedontkd@users.noreply.github.com>
2023-11-27 03:40:03 +0300

    image

--- Assets/Scripts/HP.cs
+++ Assets/Scripts/HP.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Hp : MonoBehaviour
 {
-
+    [SerializeField] private bool isPlayer;
     [SerializeField] private float maxHealth;
     
     public float Health { get; private set; }
 
     public bool IsDeath => Health <= 0;
 
+    public TMP_Text hpText;
+
+    private void Start()
+    {
+        hpText.text = ($"{Health}/{maxHealth}");
+    }
 
     private void Awake()
     {
         Health = maxHealth;
+        
     }
 
     public void TakeDamage(float damage)
@@ -22,15 +30,19 @@ public class Hp : MonoBehaviour
         Health -= damage;
         Health = Mathf.Clamp(Health, 0, maxHealth);
         print("health changed:" + damage + "health:" + Health + ", " + name);
+        if (isPlayer)
+        {
+            hpText.text = ($"{Health}/{maxHealth}");
+        }
+        
 
         if (IsDeath)
         {
             gameObject.SetActive(false);
         }
+
+    
+
     }
-
-
-
-
 
 }
