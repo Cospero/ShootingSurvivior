dd20e59ffc48b066ea86ce32161ebe5ddb473345
armagedontkd <117161052+armagedontkd@users.noreply.github.com>
2024-09-08 18:05:43 +0300

    Map+menu

---
+++ Assets/Scripts/Armagedontkd/Scripts/Scen.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Scen : MonoBehaviour
+{
+
+    [SerializeField] private GameObject MainMenu;
+    [SerializeField] private GameObject PlayMenu;
+    [SerializeField] private GameObject Shop;
+    [SerializeField] private GameObject Settings;
+
+    public void Changer(bool isShop)
+    {
+        if (isShop)
+        {
+            Shop.SetActive(!Shop.activeSelf);
+        }
+        else
+        {
+            PlayMenu.SetActive(!PlayMenu.activeSelf);
+        }
+
+        MainMenu.SetActive(!MainMenu.activeSelf);
+    }
+
+    public void Setting()
+    {
+        MainMenu.SetActive(!MainMenu.activeSelf);
+        Settings.SetActive(!Settings.activeSelf);
+    }
+
+
+    public void ChangeScene(int numberScenes)
+    {
+        SceneManager.LoadScene(numberScenes);
+    }
+
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
